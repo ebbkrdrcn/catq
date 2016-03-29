df2abf485c5e4ec9dbe483b23dcef0a5174ca339
Ebubekir Dirican <ED@Ebubekirs-MacBook-Pro.local>
2016-03-30 01:25:01 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='catq',
+    version='0.1',
+    author='Ebubekir Dirican',
+    author_email='ebubekirdirican@taracat.com.tr, ebubekirdirican@gmail.com',
+    packages=[],
+    license=' BSD license',
+    long_description="Simple Resource Query Language"
+)
